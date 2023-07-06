c3413dfe62f991e3d946187eb015c6ed87eef3e9
mia-sv <130096279+mia-sv@users.noreply.github.com>
2023-07-06 04:27:50 +0100

    tabulated find results

--- final_project.py
+++ final_project.py
@@ -15,8 +15,12 @@ MAIN_MENU = tabulate(
     tablefmt="rounded_outline",
 )
 
-API_KEY = config("API_KEY")
-API_SECRET = config("API_SECRET")
+try:
+    API_KEY = config("API_KEY")
+    API_SECRET = config("API_SECRET")
+except UndefinedValueError:
+    sys.exit("Missing API key or secret.")
+
 API_URL = "https://api.petfinder.com/v2"
 
 
@@ -39,15 +43,58 @@ def main():
                 print("Invalid option. Try again!")
 
 
+# This only works for bash terminals
+def hide_url(url):
+    return f"\u001b]8;;{url}\u001b\\Click here!\u001b]8;;\u001b\\"
+
+
 def find(token):
+    def format_animal(animal):
+        return [
+            animal["id"],
+            animal["type"],
+            animal["breeds"]["primary"],
+            animal["colors"]["primary"],
+            animal["age"],
+            animal["gender"],
+            animal["size"],
+            animal["status"],
+            hide_url(animal["url"]),
+        ]
+
     try:
-        find_animals = requests.get(
+        response = requests.get(
             f"{API_URL}/animals", headers={"Authorization": f"Bearer {token}"}
         ).json()
     except requests.RequestException:
         sys.exit("Request failed.")
 
-    print(find_animals)
+    total_count = response["pagination"]["total_count"]
+    current_page = response["pagination"]["current_page"]
+    total_pages = response["pagination"]["total_pages"]
+
+    animals = list(map(format_animal, response["animals"]))
+
+    results_table = tabulate(
+        [
+            [
+                "ID",
+                "Species",
+                "Breed",
+                "Color",
+                "Age",
+                "Gender",
+                "Size",
+                "Adoptable",
+                "URL",
+            ],
+            *animals,
+        ],
+        headers="firstrow",
+        tablefmt="rounded_outline",
+    )
+
+    print(results_table)
 
 
 def favourite(token):
@@ -81,6 +128,3 @@ def generate_token():
 
 if __name__ == "__main__":
     main()
-
-
-# user_input = input("What's your favorite pet?\n[1]Dog [2]Cat [3]Other\n")
